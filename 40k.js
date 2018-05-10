a836f3ef3336d4d68f91badc0b4c2df6c1fd8b35
Jason Wolfe <jasonwolfe@gmail.com>
2018-05-09 20:40:38 -0700

    improved variables

--- 40k.js
+++ 40k.js
@@ -1,7 +1,7 @@
 // config variables
 const CP = 14;
 const visions_sanguinus = 2;
-const relic_spends = 3;
+const relic_spends = 2;
 const opponent_strats = 8;
 const execs = 40000;
 const VERITAE = true;
@@ -13,27 +13,30 @@ const d6 = () => {
   return Math.floor(Math.random() * 6) + 1;
 };
 
-const performStrat = spend => {
+const performStrat = (spend, hits) => {
   let result = 0 - spend;
   if(GRAND_STRATEGIST){
     for (let i = 0; i < spend; i++) {
       if (d6() >= 5) {
+          hits.GRAND_STRATEGIST++;
         result++;
       }
     }
   }
   if (VERITAE) {
     if (d6() >= 5) {
+        hits.VERITAE++;
       result++;
     }
   }
   return result;
 };
 
-const kurovs = opponent_strats => {
-  let result = 0;
+const kurovs = (opponent_strats, hits) => {
+   let result = 0;
   for (let i = 0; i < opponent_strats; i++) {
     if (d6() >= 5) {
+    hits.KUROVS++;
       result++;
     }
   }
@@ -48,27 +51,52 @@ const average = ary => {
   return sum / ary.length;
 }
 
+const averageHits = ary => {
+    let VERITAE_HITS  = 0;
+    let KUROVS_HITS = 0;
+    let GRAND_STRATEGIST_HITS  = 0;
+  ary.forEach(res => {
+    VERITAE_HITS += res.VERITAE
+    KUROVS_HITS += res.KUROVS
+    GRAND_STRATEGIST_HITS += res.GRAND_STRATEGIST
+  })
+  const len = ary.length
+  return {
+      VERITAE: VERITAE_HITS/len,
+      KUROVS: KUROVS_HITS/len,
+      GRAND_STRATEGIST: GRAND_STRATEGIST_HITS/len,
+  };
+}
+
 const playGame = () => {
   const results = [];
+  const hitResults = [];
   for (let i = 0; i < execs; i++) {
     let spent = 0;
     let current = CP - visions_sanguinus - relic_spends;
-    if(KUROVS){
-      current += kurovs(opponent_strats);
+    const hits = {
+        VERITAE: 0,
+        KUROVS: 0,
+        GRAND_STRATEGIST: 0,
     }
+    if(KUROVS){
+      current += kurovs(opponent_strats, hits);
+    }
+
     while (current > 0) {
       const roll = STRATS[d6()];
       const strat = roll > current ? current : roll;
-      current += performStrat(strat);
+      current += performStrat(strat, hits);
       spent += strat;
     }
     results.push(spent);
+    hitResults.push(hits);
   }
 
-  // console.log('Results:');
-  // console.log(results);
-  console.log('average:');
+  console.log('average results:');
   console.log(average(results));
+  console.log('average hits:');
+  console.log(averageHits(hitResults));
 };
 
 playGame();
@@ -85,7 +113,4 @@ const checkRandom = () => {
   console.log(average(results));
 }
 
-checkRandom();
-
-
-
+// checkRandom();
